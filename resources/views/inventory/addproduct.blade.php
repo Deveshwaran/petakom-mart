b75dc38aa45b30f8f09063f39bafd559149d1d9e
Deveshwaran Arumugam <deveshwaran@hotmail.com>
2023-04-01 16:04:02 +0800

    first commit

---
+++ resources/views/inventory/addproduct.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.template')
+
+@section('content')
+
+<div class="pagetitle">
+  <h1>Add Product</h1>
+  <nav>
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+      <li class="breadcrumb-item active">Inventory</li>
+    </ol>
+  </nav>
+</div><!-- End Page Title -->
+
+<section class="section dashboard">
+  <div class="row">
+
+    <!-- Left side columns -->
+    <div class="col-lg-12">
+      <div class="row">
+
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Product Details</h5>
+    
+            <!-- Multi Columns Form -->
+            <form class="row g-3">
+              <div class="col-md-12">
+                <label for="productname" class="form-label">Product Name</label>
+                <input type="text" class="form-control" id="productname">
+              </div>
+              <div class="col-md-12">
+                <label for="supplier" class="form-label">Supplier</label>
+                <input type="text" class="form-control" id="supplier">
+              </div>
+              <div class="col-md-12">
+                <label for="date" class="form-label">Date Received</label>
+                <input type="date" class="form-control" id="date">
+              </div>
+              <div class="col-md-12">
+                <label for="price" class="form-label">Price (RM)</label>
+                <input type="text" class="form-control" id="price">
+              </div>
+              <div class="col-md-12">
+                <label for="stock" class="form-label">Stock</label>
+                <input type="text" class="form-control" id="stock">
+              </div>
+              <div class="text-center">
+                <button type="submit" class="btn btn-primary">Add</button>
+                <button type="reset" class="btn btn-secondary">Reset</button>
+              </div>
+            </form><!-- End Multi Columns Form -->
+    
+          </div>
+        </div>
+    
+      </div>
+    </div>
+
+  </div>
+</section>
+
+@endsection
+
+@push('scripts')
+
+@endpush
---
+++ resources/views/inventory/inventory.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.template')
+
+@section('content')
+
+<div class="pagetitle">
+  <h1>Inventory</h1>
+  <nav>
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+      <li class="breadcrumb-item active">Inventory</li>
+    </ol>
+  </nav>
+</div><!-- End Page Title -->
+
+<section class="section dashboard">
+  <div class="row">
+
+    <div class="col-lg-2 mb-3">
+        <button type="button" class="btn btn-success">
+          Add Product
+        </button>
+    </div>
+
+    <!-- Left side columns -->
+    <div class="col-lg-12">
+      <div class="row">
+
+        <!-- Recent Sales -->
+        <div class="col-12">
+          <div class="card recent-sales overflow-auto">
+
+            <div class="card-body">
+              <h5 class="card-title">All Products</span></h5>
+
+              <table class="table table-borderless datatable">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Supplier</th>
+                    <th scope="col">Date Received</th>
+                    <th scope="col">Product</th>
+                    <th scope="col">Price (RM)</th>
+                    <th scope="col">Stock</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">#2457</th>
+                    <td>Supplier</td>
+                    <td>Date Received</td>
+                    <td>Brandon Jacob</td>
+                    <td>64.00</td>
+                    <td>100</td>
+                    <td>
+                      <span class="badge bg-success">High</span>
+                      <span class="badge bg-warning">Low</span>
+                      <span class="badge bg-danger">Out of Stock</span>
+                    </td>
+                    <td>
+                      <button type="button" class="btn btn-success btn-sm"><i class="bi bi-plus-square"></i></button>
+                      <button type="button" class="btn btn-danger btn-sm"><i class="bi bi-trash"></i></button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+
+            </div>
+
+          </div>
+        </div><!-- End Recent Sales -->
+
+      </div>
+    </div>
+
+  </div>
+</section>
+
+@endsection
+
+@push('scripts')
+
+@endpush
